6bd50c548e43a79a48139d63dcffaf62b944de26
Nicolas da Silva <nicolas.dasilva88@gmail.com>
2022-06-07 22:05:31 -0300

    Preparing Challenge whit Spring

---
+++ challenge/src/test/java/ar/com/alkemy/campus/java/julio/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alkemy.campus.java.julio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
